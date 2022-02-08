3e83f6e7d1357f46dba84db67c97512d43f249a7
trevorgrabham <tgrabham@sfu.ca>
2022-02-07 20:06:20 -0800

    Changed the implementation of the coefficient vector to the storage of rows instead of columns to speed up the pivot phase. Have completed a rudementary solve function that is able to automatically solve LPs if they are in standard format, with a standard variable selection method and do not have artificial variables.

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,13 @@ struct TableauData {
     obj_numerator: i64,
     obj_denominator: i64,
     solved: bool,
+    solution_numerators: Vec<i64>,
+    solution_denominators: Vec<i64>,
 }
 
 #[derive(Debug)]
 struct Tableau {
+    DEBUG: bool,
     A: Vec<Vec<Ratio<i64>>>,
     b: Vec<Ratio<i64>>,
     c: Vec<Ratio<i64>>,
@@ -47,6 +50,7 @@ struct Tableau {
     error_message: String,
     entering_var_index: Option<usize>,
     leaving_var_index: Option<usize>,
+    solution: Vec<Ratio<i64>>,
 }
 
 impl TableauData {
@@ -65,6 +69,9 @@ impl TableauData {
         let (reduced_cost_num, reduced_cost_den): (Vec<i64>, Vec<i64>) = t.reduced_cost.into_iter()
                                                                                       .map(|el| (*el.numer(), *el.denom()))
                                                                                       .unzip();
+        let (sol_num, sol_den): (Vec<i64>, Vec<i64>) = t.solution.into_iter()
+                                                                 .map(|el| (*el.numer(), *el.denom()))
+                                                                 .unzip();
         TableauData {
             A_numerators: a_num,
             A_denominators: a_den,
@@ -84,30 +91,32 @@ impl TableauData {
             obj_denominator: *t.obj.denom(),
             basis_indecies: t.basis_indecies,
             solved: t.solved,
+            solution_numerators: sol_num,
+            solution_denominators: sol_den,
         }
     }
 }
 
 impl Tableau {
     fn new(t: TableauData) -> Tableau {
-        let mut a = vec![Vec::with_capacity(t.m);t.n];
-        let zipped_a = t.A_numerators.into_iter().zip(t.A_denominators.into_iter());
-        for (col, zipped_col) in a.iter_mut().zip(zipped_a.into_iter()) {
-            for (num, den) in zipped_col.0.into_iter().zip(zipped_col.1.into_iter()) {
-                col.push(Ratio::new(num, den));
-            }
-        }
-        let mut b = Vec::with_capacity(t.m);
-        let zipped_b = t.b_numerators.into_iter().zip(t.b_denominators.into_iter());
-        for (num, den) in zipped_b {
-            b.push(Ratio::new(num, den));
-        }
-        let mut c = Vec::with_capacity(t.n);
-        let zipped_c = t.c_numerators.into_iter().zip(t.c_denominators.into_iter());
-        for (num, den) in zipped_c {
-            c.push(Ratio::new(num, den));
-        }
+        let a: Vec<Vec<Ratio<i64>>> = t.A_numerators.into_iter()
+                              .zip(t.A_denominators.into_iter())
+                              .map(|(num_row, den_row)| num_row.into_iter()
+                                                             .zip(den_row.into_iter())
+                                                             .map(|(num, den)| Ratio::new(num, den))
+                                                             .collect())
+                              .collect();
+
+        let b: Vec<Ratio<i64>> = t.b_numerators.into_iter()
+                              .zip(t.b_denominators.into_iter())
+                              .map(|(num, den)| Ratio::new(num, den))
+                              .collect();
+        let c: Vec<Ratio<i64>> = t.c_numerators.into_iter()
+                                               .zip(t.c_denominators.into_iter())
+                                               .map(|(num, den)| Ratio::new(num, den))
+                                               .collect();
         Tableau {
+            DEBUG: true,
             A: a,
             b: b,
             c: c,
@@ -124,20 +133,54 @@ impl Tableau {
             error_message: String::from(""),
             entering_var_index: None,
             leaving_var_index: None,
+            solution: vec![Ratio::from_integer(0i64);t.n],
+        }
+
+    }
+
+    fn error_tableau(t: Tableau) -> Tableau {
+        Tableau {
+            DEBUG: true,
+            A: vec![Vec::with_capacity(0);1],
+            b: Vec::with_capacity(0),
+            c: Vec::with_capacity(0),
+            m: 0,
+            n: 0,
+            reduced_cost: Vec::with_capacity(0),
+            obj: Ratio::from_integer(-1),
+            basis_indecies: Vec::with_capacity(0),
+            has_artificial_vars: false,
+            variable_select_type: String::from(""),
+            solve_algorithm: String::from(""),
+            solved: false,
+            error: t.error,
+            error_message: t.error_message,
+            entering_var_index: None,
+            leaving_var_index: None,
+            solution: Vec::with_capacity(0),
         }
     }
 
     fn find_basis_matrix(&mut self) {
+        let mut a_cols = vec![Vec::with_capacity(self.m);self.n];
+        for row in 0..self.m {
+            for col in 0..self.n {
+                a_cols[col].push(self.A[row][col]);
+            }
+        }
         let mut I = vec![Ratio::new(0,1);self.m];
         I[0] = Ratio::new(1,1);
         for i in 0..self.m {
-            let res = self.A.iter_mut().position(|row| row == &I);
+            let res = a_cols.iter().position(|col| col == &I);
             match res {
                 Some(index) => {
                     self.basis_indecies[i] = index;
                 },
                 None => {
-                    self.A.push(I.clone());
+                    for row in &mut self.A {
+                        row.push(Ratio::new(0i64,1));
+                    }
+                    self.A[i][self.n] = Ratio::new(1i64,1);
                     self.c.push(Ratio::from_integer(i64::MIN));
                     self.basis_indecies[i] = self.n;
                     self.n += 1;
@@ -151,23 +194,16 @@ impl Tableau {
     fn calc_reduced_cost(&mut self) {
         match self.has_artificial_vars {
             false => {
-                let mut basis_cost= Vec::with_capacity(self.m);
-                for i in 0..self.m {
-                    basis_cost.push(self.c[self.basis_indecies[i]]);
+                let basis_cost:Vec<Ratio<i64>> = self.basis_indecies.iter()
+                                                                    .map(|&index| self.c[index])
+                                                                    .collect();
+                for col in 0..self.n {
+                    self.reduced_cost.push(Ratio::new(0i64, 1));
+                    for row in 0..self.m {
+                        self.reduced_cost[col] += self.A[row][col] * basis_cost[row];
+                    }
+                    self.reduced_cost[col] -= self.c[col];
                 }
-                let mut sums: Vec<Ratio<i64>> = Vec::with_capacity(self.n);
-                for row in &self.A {
-                    sums.push(row
-                                .iter()
-                                .zip(basis_cost.iter())
-                                .map(|(r, c)| r*c )
-                                .sum());
-                }
-                self.reduced_cost = sums.iter()
-                    .zip(self.c.iter())
-                    .map(|(sum, c)| sum - c)
-                    .collect();
-
                 self.obj = basis_cost.iter()
                                     .zip(self.b.iter())
                                     .map(|(c, b)| c*b)
@@ -184,6 +220,7 @@ impl Tableau {
                 match self.solve_algorithm.as_str() {
                     "standard" => {
                         let min_value = self.reduced_cost.iter().min();
+                        // match on 3 cases: we have a negative value for our min reduced cost, we have a positive value for our min reduced cost, or we have an empty reduced cost 
                         match min_value {
                             Some(min_v) if min_v < &Ratio::from_integer(0i64) => {
                                 let min_index = self.reduced_cost.iter()
@@ -255,14 +292,23 @@ impl Tableau {
                             },
                             Some(index) => index
                         };
-                        let ratios: Vec<Ratio<i64>> = self.A[entering_index].iter()
-                                                           .zip(self.b.iter())
-                                                           .map(|(&a, &b)| b/a)
-                                                           .collect();
+                        let ratios: Vec<Ratio<i64>> = self.A.iter()
+                                                            .map(|row| row[entering_index])
+                                                            .zip(self.b.iter())
+                                                            .map(|(a, b)| if a != Ratio::from_integer(0i64) { b/a } else { Ratio::new(i64::MAX, 1) })
+                                                            .collect();
                         let min_ratio = ratios.iter()
-                                              .filter(|&&el| el > Ratio::from_integer(0i64))
-                                              .min()
-                                              .unwrap();
+                                              .filter(|&&el| el >= Ratio::from_integer(0i64))
+                                              .min();
+                        let min_ratio = match min_ratio {
+                            Some(ratio) => ratio,
+                            None => {
+                                self.error = true;
+                                self.error_message = String::from("Problem is unbounded.");
+                                self.leaving_var_index = None;
+                                return;
+                            }
+                        };
                         let min_index = ratios.iter()
                                               .position(|el| el == min_ratio)
                                               .unwrap();
@@ -295,19 +341,78 @@ impl Tableau {
     }
 
     fn pivot(&mut self) {
-
+        match (self.entering_var_index, self.leaving_var_index) {
+            (Some(entering_index), Some(leaving_index)) => {
+                let leaving_row = &self.A[leaving_index];
+                let entering_col: Vec<Ratio<i64>> = self.A.iter()
+                                         .map(|row| row.iter().cloned().nth(entering_index).unwrap())
+                                         .collect();
+                let new_a: Vec<Vec<Ratio<i64>>> = self.A.iter()
+                                                        .zip(entering_col.iter())
+                                                        .map(|(row, &entering_el)| row.iter()
+                                                                                        .zip(leaving_row.iter())
+                                                                                        .map(|(row_el, &leaving_el)| if row == leaving_row { row_el / entering_el } else { row_el - leaving_el * entering_el / self.A[leaving_index][entering_index]})
+                                                                                        .collect::<Vec<Ratio<i64>>>())
+                                                        .collect();
+                let new_reduced_cost: Vec<Ratio<i64>> = self.reduced_cost.iter()
+                                                                         .zip(leaving_row.iter())
+                                                                         .map(|(reduced_el, &leaving_el)| reduced_el - leaving_el * self.reduced_cost[entering_index] / self.A[leaving_index][entering_index])
+                                                                         .collect();
+                let new_b: Vec<Ratio<i64>> = self.b.iter()
+                                                   .zip(entering_col.iter())
+                                                   .zip(0..self.m)
+                                                   .map(|((b_el, entering_el), index)| if index == leaving_index { b_el / self.A[leaving_index][entering_index] } else { b_el - entering_el * self.b[leaving_index] / self.A[leaving_index][entering_index] })
+                                                   .collect();
+                self.obj -= self.b[leaving_index] * self.reduced_cost[entering_index] / self.A[leaving_index][entering_index];
+                self.A = new_a;
+                self.reduced_cost = new_reduced_cost;
+                self.b = new_b;
+                self.basis_indecies[leaving_index] = entering_index;
+                ()
+            },
+            (Some(_), None) => {
+                self.error = true;
+                self.error_message = String::from("No leaving variable has been identified.");
+                ()
+            },
+            (None, Some(_)) => {
+                self.error = true;
+                self.error_message = String::from("No entering variable has been identified.");
+                ()
+            }, 
+            (None, None) => {
+                self.error = true;
+                self.error_message = String::from("No entering or leaving variables have been identified.");
+                ()
+            },
+        }
     }
 
     fn iterate(&mut self) {
         match self.solve_algorithm.as_str() {
             "standard" => {
                 self.select_entering_var(); 
+                if self.solved || self.error {
+                    return;
+                }
                 self.select_leaving_var(); 
+                if self.solved || self.error {
+                    return;
+                }
+                if self.DEBUG {
+                    println!("Entering index: {:?}\nLeaving index: {:?}\n", self.entering_var_index, self.leaving_var_index);
+                }
                 self.pivot();
             },
             "dual" => {
                 self.select_leaving_var(); 
+                if self.solved || self.error {
+                    return;
+                }
                 self.select_entering_var(); 
+                if self.solved || self.error {
+                    return;
+                }
                 self.pivot();
             },
             _ => { 
@@ -317,11 +422,16 @@ impl Tableau {
         }
     }
 
+    fn setup(&mut self) {
+        self.find_basis_matrix();
+        self.calc_reduced_cost();
+    }
+
     fn print_table(&self) {
         for row in 0..self.m {
             print!("[\t");
             for col in 0..self.n {
-                print!("{}\t", self.A[col][row]);
+                print!("{}\t", self.A[row][col]);
             }
             print!("|\t{}\t", self.b[row]);
             println!("]");
@@ -341,25 +451,35 @@ impl Tableau {
         println!("]\n");
     }
 
-    fn get_basis(&self) -> (usize, usize, usize) {
-        (self.basis_indecies[0], self.basis_indecies[1], self.basis_indecies[2])
+    fn get_solution(&mut self) {
+        for (i, &basis_index) in self.basis_indecies.iter().enumerate() {
+            self.solution[basis_index] = self.b[i];
+        }
     }
 }
 
 
-#[post("/setup", format = "json", data = "<tableau>")]
-fn setup(tableau: Json<TableauData>) -> Json<TableauData> {
+#[post("/solve", format = "json", data = "<tableau>")]
+fn solve(tableau: Json<TableauData>) -> Json<TableauData> {
     let mut t = Tableau::new(tableau.0);
-    t.find_basis_matrix();
-    t.calc_reduced_cost();
-    t.select_entering_var();
-    t.select_leaving_var();
-    t.print_table();
-    println!("Entering variable index: {}\nLeaving variable index: {}", t.entering_var_index.unwrap(), t.leaving_var_index.unwrap());
-    let basis_indecies = t.get_basis();
-    Json(TableauData::new(t))
+    t.setup();
+    for _ in 0..t.n + 1 {
+        if t.solved || t.error { break; }
+        t.print_table();
+        t.iterate();
+    }
+    if t.solved {
+        t.get_solution();
+        Json(TableauData::new(t))
+    } else if t.error {
+        Json(TableauData::new(Tableau::error_tableau(t)))
+    } else {
+        t.error = true; 
+        t.error_message = String::from("Stuck in a cycle, terminted solution process.");
+        Json(TableauData::new(t))
+    }
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![setup]).launch();
+    rocket::ignite().mount("/", routes![solve]).launch();
 }
